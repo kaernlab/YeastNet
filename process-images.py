f9630b269e86d06bdba7a060decfe268e6fd7008
Danny <danny.salem2@gmail.com>
2018-10-02 12:41:29 -0400

    image construction and output is done

--- process-images.py
+++ process-images.py
@@ -1,9 +1,22 @@
 import numpy
-from scipy import misc
-from matplotlib import pyplot 
+import imageio
+import matplotlib.pyplot as plt
 
-image = misc.imread('..\\NewSegData\\XY2\\z2_t_000_000_001_BF.tif')
 
-print(image.size)
+#Load all z stacks
+image = imageio.imread('../NewSegData/XY2/z1_t_000_000_005_BF.tif')
+image1 = imageio.imread('../NewSegData/XY2/z2_t_000_000_005_BF.tif')
+image2 = imageio.imread('../NewSegData/XY2/z3_t_000_000_005_BF.tif')
 
-pyplot.imshow(image, cmap=pyplot.cm.gray)  
+#Rescale images to 0-1
+image = numpy.true_divide(image - image.min(), image.max() - image.min())
+image1 = numpy.true_divide(image1 - image1.min(), image1.max() - image1.min())
+image2 = numpy.true_divide(image2 - image2.min(), image2.max() - image2.min())
+
+#Stack the 3 zstacks into a 3 channels of an rgb image
+image3 = numpy.dstack((image,image1,image2))
+
+#Display image
+plt.figure()
+plt.imshow(image3)  
+plt.show()
