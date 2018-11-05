f3c06ce78f74a2771baac178677da1808f61064a
HomePC <rikku007@gmail.com>
2018-11-05 00:49:17 -0500

    added batch saving

--- trainNetwork.py
+++ trainNetwork.py
@@ -6,6 +6,7 @@ import tensorboardX as tbX
 import pdb
 import random
 import time
+import numpy
 
 ## Import Custom Modules
 import processImages as pi
@@ -22,15 +23,31 @@ start_time = time.time()
 ## Launch Tensorboard Summary Writing Object
 writer = tbX.SummaryWriter()#log_dir="./logs")
 
-## Make Test and Validation Partitions
-samplingList = list(range(pi.num_images()))
-samples = random.sample(samplingList,153)
-trainingIDs = samples[:129]
-testIDs = samples[130:]
+
+
+iteration = 0
+start = 0
+resume = True
+if resume:
+    start = 1803
+    iteration = 30634
+    batchIDs = numpy.load('batchIDs.npy').item()
+else:
+    ## Make Test and Validation Partitions
+    samplingList = list(range(pi.num_images()))
+    samples = random.sample(samplingList,153)
+    trainingIDs = samples[:129]
+    testIDs = samples[130:]
+    batchIDs = {
+        "train": trainingIDs,
+        "test": testIDs
+    } 
+    numpy.save('batchIDs.npy', batchIDs) #Need to do .item() when loading
+
 
 ## Instantiate Net, Load Parameters, Move Net to GPU
 net = Net()
-optimizer = optim.SGD(net.parameters(), lr=0.1)
+optimizer = optim.SGD(net.parameters(), lr=0.01)#, momentum=0.9)
 
 ##Send Model to GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -41,11 +58,11 @@ net.load_state_dict(torch.load("model.pt"))
 optimizer.load_state_dict(torch.load("model_opt.pt"))
 
 ## Instantiate Training and Validation DataLoaders
-trainDataSet = YeastSegmentationDataset(trainingIDs, crop_size = 128)
-trainLoader = torch.utils.data.DataLoader(trainDataSet, batch_size=4,
+trainDataSet = YeastSegmentationDataset(batchIDs['train'], crop_size = 256)
+trainLoader = torch.utils.data.DataLoader(trainDataSet, batch_size=8,
                                           shuffle=True, num_workers=0)
 
-testDataSet = YeastSegmentationDataset(testIDs, crop_size = 512)
+testDataSet = YeastSegmentationDataset(batchIDs['test'], crop_size = 512)
 testLoader = torch.utils.data.DataLoader(testDataSet, batch_size=1,
                                          shuffle=False, num_workers=0)
 
@@ -54,10 +71,11 @@ testLoader = torch.utils.data.DataLoader(testDataSet, batch_size=1,
 #optimizer = optim.SGD(net.parameters(), lr=0.1)#, momentum=0.9, weight_decay=0.0005)
 criterion = WeightedCrossEntropyLoss()#nn.CrossEntropyLoss()#
 classes = ('background','cell')
-iteration = 0
+
+
 
 # Epoch Loop: first loops over batches, then over v alidation set
-for epoch in range(100):  
+for epoch in range(start,10000):  
     
     ## Batch Loop
     for i, data in enumerate(trainLoader, 0):
@@ -105,9 +123,10 @@ for epoch in range(100):
     #if True #saveCP:
     torch.save(net.state_dict(),  "model.pt")
     torch.save(optimizer.state_dict(),  "model_opt.pt")
+    #numpy.save('batchIDs.npy', resume)
 
 ## Finish
 elapsed_time = time.time() - start_time
-print('Finished Training, Duration: seconds' + str(format(elapsed_time, '02d')))
+print('Finished Training, Duration: seconds' + str(elapsed_time))#format(elapsed_time, '02d')))
 writer.close()
 torch.save(net.state_dict(),  "model.pt")
