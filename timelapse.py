c8ec066ffb80afe4f2bb9fb6d905f7dabdc07b17
Salem <Danny.Salem@ds.nrc.ca>
2018-11-06 18:55:37 -0500

    Tracking mostly working. Need to fix bug with background label persisting in pipeline

--- timelapse.py
+++ timelapse.py
@@ -3,6 +3,9 @@ import numpy as np
 import torch
 import torchvision as tv
 import os
+import pdb
+import scipy.spatial.distance as scipyD
+import scipy.optimize as scipyO
 
 
 class Timelapse():
@@ -11,12 +14,14 @@ class Timelapse():
         self.toTensor = tv.transforms.ToTensor()
         self.image_dir = image_dir
         self.num_images = len(os.listdir(self.image_dir))
+        self.total_cells = 0
 
         self.tensorsBW = [None] * self.num_images
         self.imagesBW = [None] * self.num_images#np.array([self.num_images])
         self.masks = [None] * self.num_images#np.array([self.num_images])
         self.labels = [None] * self.num_images#np.array([self.num_images])
         self.centroids = [None] * self.num_images#np.array([self.num_images])
+        self.identity = [None] * self.num_images
 
     def loadImages(self, dimensions = 1024, normalize = False):
         for idx, image_name in enumerate(os.listdir(self.image_dir)):
@@ -52,3 +57,24 @@ class Timelapse():
         image = (image - mean) / std
         image = (image - image.min()) / image.max()
         return image
+
+    def cellTrack(self):
+        self.identity[0] = np.unique(self.labels[0])[:]
+        self.total_cells = len(self.identity[0])
+
+        for idx, (first, second) in enumerate(zip(self.centroids[:-1], self.centroids[1:])):
+            timepoint = idx+1
+            Y = scipyD.cdist(first, second, 'euclidean')
+            firstLabels, secondLabels = scipyO.linear_sum_assignment(Y)
+            firstLabels = self.identity[timepoint-1]
+            self.identity[timepoint] = np.full(len(second), -1)
+            for idx, label in enumerate(secondLabels):
+                self.identity[timepoint][label] = firstLabels[idx]
+            #pdb.set_trace()
+            for idx, label in enumerate(self.identity[timepoint]):
+                if label == -1:
+                    self.identity[timepoint][idx] = self.total_cells
+                    self.total_cells += 1
+            
+
+            
--- trackingPipeline.py
+++ trackingPipeline.py
@@ -28,9 +28,18 @@ tl.makeMasks(masks)
 # Pass Mask into cell labeling script, return labelled cells 
 for idx, (imageBW, mask) in enumerate(zip(tl.imagesBW, tl.masks)):
     tl.centroids[idx], x, tl.labels[idx] = labelCells.label_cells(np.array(mask).astype(np.uint8), np.array(imageBW))
-    imageio.imwrite('inference/label' + str(idx) + '.png', np.array(tl.labels[idx]))
-    imageio.imwrite('inference/overlay' + str(idx) + '.png', x)
+    #imageio.imwrite('inference/label' + str(idx) + '.png', np.array(tl.labels[idx]))
+    #imageio.imwrite('inference/overlay' + str(idx) + '.png', x)
 
+
+tl.cellTrack()
+
+for imageID in range(tl.num_images-1):
+    plt.figure()
+    for idx, (label, cnt) in enumerate(zip(tl.identity[imageID], tl.centroids[imageID])):
+        plt.imshow(tl.imagesBW[imageID])
+        plt.text(cnt[0]-2, cnt[1]+2, label, fontsize=8, color='r')
+    plt.show()
 
 # Test Image
 #plt.figure()
