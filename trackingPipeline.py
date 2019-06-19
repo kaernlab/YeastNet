6f96fd4088e8bd878570c1797e18add02dd422f1
Danny Salem <danny.salem2@gmail.com>
2019-06-19 14:13:03 -0400

    Extend trackingPipeline.py with more arguments
    To make comparisons of the accuracy of the different methods, accuracy
    needs to be measured and output. flags were added to allow the user
    to output necessary csv files.

--- trackingPipeline.py
+++ trackingPipeline.py
@@ -1,15 +1,20 @@
 import argparse
 import pdb
 from Utils.makeTimelapse import makeTimelapse
+from Utils.TestPerformance import makeResultsCSV, getMeanAccuracy
 
 ## Parse Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--imagefolder", type=str, help="Input string of image folder name. Default is current Directory", default="./")
-parser.add_argument("--make_plot", type=str, help="Boolean variable to choose whether fluorescence plots are desired", default=False)
+parser.add_argument("-f","--imagefolder", type=str, help="Input string of image folder name. Default is current Directory", default="./")
+parser.add_argument("-p","--make_plot", type=bool, help="Boolean variable to choose whether fluorescence plots are desired", default=False)
+parser.add_argument("-c","--make_csv", type=bool, help="Boolean variable to choose whether csv is made for Benchmarking", default=False)
+parser.add_argument("-i","--compare_iou", type=bool, help="Boolean variable to choose whether IOU measurement is made", default=False)
 args = parser.parse_args()
 
 imagefolder = args.imagefolder
 makePlots = args.make_plot
+makeCSV = args.make_csv
+compareIOU = args.compare_iou
 model_path = './ynetmodel/trainedModel.pt'
 
 ## For testing purposes
