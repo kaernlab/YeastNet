762b8225767b056300a66eb821f689df793c8546
Danny Salem <danny.salem2@gmail.com>
2019-06-27 12:04:34 -0400

    extend the dataset class to include more datasets
    Given a folder containing more than one dataset, the dataset class will
    return the correct number of images and will convert an index to the
    right image ID from different datasets. Extendable to more images and
    more datasets with minimal work.

--- ynetmodel/YeastSegmentationDataset.py
+++ ynetmodel/YeastSegmentationDataset.py
@@ -14,30 +14,44 @@ import torchvision.transforms.functional as TF
 #Define Dataset Class
 class YeastSegmentationDataset(Dataset):
 
-    def __init__(self, list_IDs, transform=None, crop_size = 512, random_rotate = False, random_flip = False, no_og_data = False):
+    def __init__(self, list_IDs, transform=None, crop_size = 512, random_rotate = False, random_flip = False, random_crop = False, no_og_data = False):
         self.ToTensor = TV.ToTensor()
-        self.ToPILImage = TV.ToPILImage()
         self.crop_size = crop_size
         self.list_IDs = list_IDs
-        self.all_data = list(range(len(os.listdir('Training Data 1D/Images'))))
-        self.rotate = random_rotate
-        self.flip = random_flip
+        self.to_rotate = random_rotate
+        self.to_flip = random_flip
+        self.to_crop = random_crop
         self.no_og_data = no_og_data
 
     def __len__(self):
-        return len(self.list_IDs)
+        return sum([len(x) for x in self.list_IDs.values()])
 
-    def __getitem__(self,idx):
-        data_id = self.all_data[self.list_IDs[idx]]
-        mask = self.centerCrop(self.loadMask(data_id),self.crop_size)
-        weight_loss_matrix = self.centerCrop(self.loadLossMap(data_id), self.crop_size)
-        bw_image = self.loadImage(data_id)       
-        bw_image = self.centerCrop(self.normalize(bw_image, bw_image.mean(), bw_image.std()), self.crop_size)
+    def __getitem__(self,test_ID):
 
-        if self.rotate:
+        dataset_names = list(self.list_IDs) # should be ['DSDataset],[YITDataset1],[YITDataset3]
+        true_ID = test_ID
+        true_dataset = dataset_names[0]
+
+        for current_dataset, next_dataset in zip(dataset_names[:-1], dataset_names[1:]):
+            dataset_size = len(self.list_IDs[current_dataset])
+            if true_ID >= dataset_size:
+                true_ID -= dataset_size
+                true_dataset = next_dataset
+            else:
+                break
+        true_ID = self.list_IDs[true_dataset][true_ID]
+        data_ID = (true_ID, true_dataset)
+        mask = self.loadMask(data_ID)
+        weight_loss_matrix = self.loadLossMap(data_ID)
+        bw_image = self.loadImage(data_ID)       
+        bw_image = self.normalize(bw_image, bw_image.mean(), bw_image.std())
+
+        if self.to_rotate:
             bw_image, mask, weight_loss_matrix = self.randomRotate(bw_image, mask, weight_loss_matrix, self.no_og_data)
-        if self.flip:
+        if self.to_flip:
             bw_image, mask, weight_loss_matrix = self.randomFlip(bw_image, mask, weight_loss_matrix)
+        if self.to_crop:
+            bw_image, mask, weight_loss_matrix = self.randomCrop(bw_image, mask, weight_loss_matrix, self.crop_size)
 
         bw_image = self.ToTensor(bw_image)
         return bw_image, mask, weight_loss_matrix
@@ -56,18 +70,18 @@ class YeastSegmentationDataset(Dataset):
         image = (image - image.min()) / image.max()
         return image
 
-    def loadImage(self, idx):
-        image = imageio.imread('Training Data 1D/Images/im%03d.tif' % idx) 
+    def loadImage(self, dataID):
+        image = imageio.imread('./Datasets/' + dataID[1] + '/Images/im%03d.tif' % dataID[0]) 
         return image[:,:,numpy.newaxis].astype(numpy.double)
 
-    def loadMask(self, idx):
-        mask = sio.loadmat('Training Data 1D/Masks/mask' + str(idx) + '.mat')
-        mask = (mask['LAB'] != 0)*1
+    def loadMask(self, dataID):
+        mask = numpy.load('./Datasets/' + dataID[1] + '/Masks/mask%03d.npy' % dataID[0]) 
+        mask = (mask != 0)*1
         return mask[:,:,numpy.newaxis]
         
-    def loadLossMap(self, idx):
-        weightmap = numpy.load('Training Data 1D/Loss Weight Maps/lwm' + str(idx) + '.npy')
-        return weightmap[:,:,numpy.newaxis].astype(numpy.float32)
+    def loadLossMap(self, dataID):
+        weightMap = numpy.load('./Datasets/' + dataID[1] + '/LossWeightMaps/lwm%03d.npy' % dataID[0]) 
+        return weightMap[:,:,numpy.newaxis].astype(numpy.float32)
 
     def showImage(self, image):
         #Display image
@@ -81,7 +95,6 @@ class YeastSegmentationDataset(Dataset):
         else:
             angle = random.choice([1, 2, 3, 4])
 
-        
         for rotations in range(angle):
             bw_image = numpy.rot90(bw_image) - numpy.zeros_like(bw_image)
             mask = numpy.rot90(mask) - numpy.zeros_like(mask)
@@ -89,11 +102,12 @@ class YeastSegmentationDataset(Dataset):
         
         return bw_image, mask, loss_map
 
-    def random_sample(self, bw_image, mask, loss_map):
-        x,y = int(numpy.random.randint((mask.shape[0]-161), size=1)), int(numpy.random.randint((mask.shape[0]-161), size=1))
-        bw_image = bw_image[x:x+160,y:y+160]
-        mask = mask[x:x+160,y:y+160]
-        loss_map = loss_map[x:x+160,y:y+160]
+    def randomCrop(self, bw_image, mask, loss_map, crop_size):
+        if mask.shape[0] > crop_size:
+            x,y = int(numpy.random.randint((mask.shape[0]-(crop_size+1)), size=1)), int(numpy.random.randint((mask.shape[1]-(crop_size+1)), size=1))
+            bw_image = bw_image[x:x+crop_size,y:y+crop_size]
+            mask = mask[x:x+crop_size,y:y+crop_size]
+            loss_map = loss_map[x:x+crop_size,y:y+crop_size]
         return bw_image, mask, loss_map
 
     def randomFlip(self, bw_image, mask, loss_map):
