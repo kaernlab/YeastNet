2b4e1646fc519a4b324a24ec657b9953db8b36ff
Danny Salem <danny.salem2@gmail.com>
2019-12-20 13:21:59 -0500

    edit train script to allow for hyperparameter testing

--- trainNetwork.py
+++ trainNetwork.py
@@ -23,26 +23,29 @@ from ynetmodel.WeightedCrossEntropyLoss import WeightedCrossEntropyLoss
 def main():
 ## Set K-fold, checking if its an environment variable
     try:
-        k = int(os.environ['K_FOLD'])
-        toResume = os.environ['RESUME']
-        normtype = os.environ['NORMTYPE']
-        allDatasets = os.environ['ALLDATASETS']
-        loss_param = [os.environ['W0'],os.environ['SIGMA']]
+    #    k = int(os.environ['K_FOLD'])
+    #    toResume = os.environ['RESUME']
+    #    normtype = os.environ['NORMTYPE']
+    #    allDatasets = os.environ['ALLDATASETS']
+        #loss_param = [os.environ['W0'],os.environ['SIGMA']]
+        dataset = os.environ['DATASET']
     except KeyError:
-        k = 5
-        toResume = 'False'
-        normtype = 3
-        allDatasets = 'True'
+        #k = 5
+        #toResume = 'False'
+        #normtype = 3
+        #allDatasets = 'True'
         loss_param = ['10','5']
-        trainTwo = 'True'
+    toResume = 'False' 
+    trainTwo = 'True'
+    loss_param = ['10','5'] if dataset == 'DSDataset' else ['5','5']
 
     #print('Training K-fold ' + str(k) + ': Normalization type ' + str(normtype))
-    print('Training with w0={}, sigma={}. Normalization type: {}'.format(loss_param[0], loss_param[1], normtype))
+    print('Training with w0={}, sigma={}. Dataset: {}'.format(loss_param[0], loss_param[1], dataset),flush=True)
 
 
     ## Start Timer, Tensorboard
     start_time = time.time()
-    end = 2
+    end = 1000
     writer = SummaryWriter(comment='_12test3,w0={},sigma={}'.format(loss_param[0], loss_param[1]))#log_dir="./logs")
 
     ## Instantiate Net, Load Parameters, Move Net to GPU
@@ -88,17 +91,19 @@ def main():
         #setMoments = getDatasetMoments(trainIDs)
 
         ## New two Dataset Training
-        if trainTwo == 'True':
-            trainIDs = {
-                'DSDataset': list(range(150)),
-                'YITDataset1': list(range(60)),
-                #'YITDataset3': list(range(20)),
-            }
-            testIDs = {
-                #'DSDataset': list(range(150)),
-                #'YITDataset1': list(range(60)),
-                'YITDataset3': list(range(20)),
-            }
+        #if trainTwo == 'True':
+        #    trainIDs = {
+        #        'DSDataset': list(range(150)),
+        #        'YITDataset1': list(range(60)),
+        #        #'YITDataset3': list(range(20)),
+        #    }
+        #    testIDs = {
+        #        #'DSDataset': list(range(150)),
+        #        #'YITDataset1': list(range(60)),
+        #        'YITDataset3': list(range(20)),
+        #    }
+        trainIDs = {dataset: torch.load('./Utils/TrainingSplits/trainIDs.pt')[dataset]}
+        testIDs = {dataset: torch.load('./Utils/TrainingSplits/testIDs.pt')[dataset]}
         trainSetMoments = getDatasetMoments(trainIDs)
         testSetMoments = getDatasetMoments(testIDs)
 
@@ -111,17 +116,17 @@ def main():
     #    g['lr'] = 0.05
         #g['momentum'] = 0.9
 
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8, patience=25, verbose=True)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8, patience=50, verbose=True)
 
 
     ## Instantiate Training and Validation DataLoaders
     trainDataSet = YeastSegmentationDataset(trainIDs, crop_size = 256, random_rotate = True, random_flip = False,
-                                            no_og_data = False, random_crop=True, normtype=normtype,
+                                            no_og_data = False, random_crop=False,
                                             setMoments = trainSetMoments, loss_param=loss_param)
-    trainLoader = torch.utils.data.DataLoader(trainDataSet, batch_size=8,
+    trainLoader = torch.utils.data.DataLoader(trainDataSet, batch_size=1,
                                             shuffle=True, num_workers=0)
                                             
-    testDataSet = YeastSegmentationDataset(testIDs, normtype=normtype, setMoments = testSetMoments)
+    testDataSet = YeastSegmentationDataset(testIDs, setMoments = testSetMoments)
     testLoader = torch.utils.data.DataLoader(testDataSet, batch_size=1,
                                             shuffle=False, num_workers=0)
 
@@ -137,7 +142,6 @@ def main():
         for i, data in enumerate(trainLoader, 0):
             ## Total iteration
             iteration+=1
-
             ## Get inputs
             trainingImage, mask, lossWeightMap = data
             trainingImage, mask, lossWeightMap = trainingImage.to(device), mask.to(device), lossWeightMap.to(device)
@@ -165,15 +169,15 @@ def main():
 
         ## Epoch validation
 
-        val_acc = validateNetwork.validate(net, device, testLoader, criterion, saveImages = True)
+        val_acc = validateNetwork.validate(net, device, testLoader, criterion, saveImages = False)
         scheduler.step(val_acc)
         #print('Current LR is: ' + str(optimizer.param_groups['lr']))
         
-        print('[%d, %d] IntOfUnion (Cell): %.5f \n' % (iteration, epoch + 1, val_acc))
+        print('[%d, %d] IntOfUnion (Cell): %.5f \n' % (iteration, epoch + 1, val_acc),flush=True)
         writer.add_scalar('Validation Cell IOU', val_acc, global_step=epoch, walltime=time.time())
         ## Epoch Time
         elapsed_time = time.time() - start_time
-        print(str(elapsed_time / 60) + 'min')
+        print(str(elapsed_time / 60) + 'min',flush=True)
 
         if val_acc > highestAccuracy:
             save_option = True
@@ -202,12 +206,12 @@ def main():
             #else:
             #    outputpath ="./NewNormModels/new_norm_testV%01dK%01d.pt" % (normtype,k) 
             
-            outputpath = '12test3_w0={}_sigma={}.pt'.format(loss_param[0], loss_param[1])
+            outputpath = '{}_w0={}_sigma={}.pt'.format(dataset, loss_param[0], loss_param[1])
             torch.save(checkpoint, outputpath)
 
     ## Finish
     elapsed_time = time.time() - start_time
-    print('Finished Training, Duration: seconds' + str(elapsed_time))
+    print('Finished Training, Duration: seconds' + str(elapsed_time),flush=True)
     with open('ScreenOutput.txt','a') as outputfile:
         outputfile.writelines('w0={},sigma={},accuracy={:.4f}\n'.format(loss_param[0], loss_param[1], 
                                                                 checkpoint['highestAccuracy']))
