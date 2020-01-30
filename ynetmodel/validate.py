383063f37044014cc51e1d98cb2d79ad8d53a405
Danny Salem <danny.salem2@gmail.com>
2020-01-30 12:55:11 -0500

    change file and variable names

--- ynetmodel/validate.py
+++ ynetmodel/validate.py
@@ -7,7 +7,7 @@ import pdb
 ## Import Custom Code
 from Utils.helpers import accuracy
 
-def validate(net, device, testLoader, criterion = None, saveImages = False):
+def infer(net, device, testLoader, criterion = None, saveImages = False):
 
     ## Run net without regularization techniques
     net.eval()
@@ -15,17 +15,15 @@ def validate(net, device, testLoader, criterion = None, saveImages = False):
     ## Loss Sum accumulator for output
     runningIOU = 0
 
-
-
     ## Loop over batches
     for i, data in enumerate(testLoader, 1):
         ## Get inputs and transfer to GPU
-        validationImage, mask, _ = data
-        validationImage = validationImage.to(device)
+        image, mask, _ = data
+        image = image.to(device)
        
         ## Run Batch through data with no grad
         with torch.no_grad():
-            outputs = net(validationImage.float())
+            outputs = net(image.float())
         predictions = outputs.cpu().detach().numpy()[0,:,:,:]
         maskPrediction = (predictions[1] > predictions[0]) * 1
 
@@ -36,8 +34,8 @@ def validate(net, device, testLoader, criterion = None, saveImages = False):
         ## Output Images
         if saveImages:
             #pdb.set_trace()
-            imageio.imwrite('Validation/' + str(i) + 'Pred.png', (maskPrediction.astype('uint8'))* 255)#.astype(float) * 255))#.astype('uint8')
-            imageio.imwrite('Validation/' + str(i) + 'IMG.png', (validationImage[0,0,:,:].cpu().detach().numpy() * 255).astype('uint8'))
+            imageio.imwrite('Validation/' + str(i) + 'Pred.png', (maskPrediction.astype('uint8'))* 255)
+            imageio.imwrite('Validation/' + str(i) + 'IMG.png', (image[0,0,:,:].cpu().detach().numpy() * 255).astype('uint8'))
             imageio.imwrite('Validation/' + str(i) + 'True.png', (mask[0,:,:,0].cpu().detach().numpy() * 255).astype('uint8'))
 
     ## Return the mean Loss
