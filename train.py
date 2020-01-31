2e0824086f62555654e4a0bde03e6b9bc347a36a
Danny Salem <danny.salem2@gmail.com>
2020-01-31 17:29:45 -0500

    remove unused code from train.py

--- train.py
+++ train.py
@@ -7,51 +7,27 @@ import matplotlib.pyplot as plt
 import os
 
 from torch.utils.data import DataLoader
-from torch import optim
 from torch.utils.tensorboard import SummaryWriter
 
 ## Import Custom Modules
 from ynetmodel.detect import validate
 from Utils.helpers import getDatasetMoments
-
-## Import Custom Classes
-from ynetmodel.YeastSegmentationDataset import YeastSegmentationDataset
 from ynetmodel.defineNetwork import Net
 from ynetmodel.WeightedCrossEntropyLoss import WeightedCrossEntropyLoss
-
+from ynetmodel.YeastSegmentationDataset import YeastSegmentationDataset
 
 def main():
-## Set K-fold, checking if its an environment variable
-    try:
-    #    k = int(os.environ['K_FOLD'])
-    #    toResume = os.environ['RESUME']
-    #    normtype = os.environ['NORMTYPE']
-    #    allDatasets = os.environ['ALLDATASETS']
-        #loss_param = [os.environ['W0'],os.environ['SIGMA']]
-        dataset = os.environ['DATASET']
-    except KeyError:
-        #k = 5
-        #toResume = 'False'
-        #normtype = 3
-        #allDatasets = 'True'
-        loss_param = ['10','5']
-    toResume = 'False' 
-    trainTwo = 'True'
-    loss_param = ['10','5'] if dataset == 'DSDataset' else ['5','5']
 
-    #print('Training K-fold ' + str(k) + ': Normalization type ' + str(normtype))
+    ## Print Name/Info about Model Training Session
     print('Training with w0={}, sigma={}. Dataset: {}'.format(loss_param[0], loss_param[1], dataset),flush=True)
-
 
     ## Start Timer, Tensorboard
     start_time = time.time()
-    end = 1000
     writer = SummaryWriter(comment='_12test3,w0={},sigma={}'.format(loss_param[0], loss_param[1]))#log_dir="./logs")
 
-    ## Instantiate Net, Load Parameters, Move Net to GPU
+    ## Instantiate Net/Optimizer
     net = Net()
-    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
-
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=momentum)
 
     ##Send Model to GPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -73,22 +49,16 @@ def main():
         highestAccuracy = checkpoint['highestAccuracy']
         net.load_state_dict(checkpoint['network'])
         optimizer.load_state_dict(checkpoint['optimizer'])
-        setMoments = getDatasetMoments(trainIDs)
+
+        ##Change Optimizer params
+        #for g in optimizer.param_groups:
+        #    g['lr'] = 0.05
+        #    g['momentum'] = 0.9
     else:
         ## Intialize State
         iteration = 0
         start = 0
         highestAccuracy = 0 
-        #testIDs = torch.load("./CrossValidation/testIDs.pt")[k-1]
-        #trainIDs = torch.load("./CrossValidation/trainIDs.pt")[k-1]
-
-        #if allDatasets == 'False':
-        #    testIDs.pop('YITDataset1', None)
-        #    testIDs.pop('YITDataset3', None)
-        #    trainIDs.pop('YITDataset1', None)
-        #    trainIDs.pop('YITDataset3', None)
-
-        #setMoments = getDatasetMoments(trainIDs)
 
         ## New two Dataset Training
         #if trainTwo == 'True':
@@ -104,20 +74,16 @@ def main():
         #    }
         trainIDs = {dataset: torch.load('./Utils/TrainingSplits/trainIDs.pt')[dataset]}
         testIDs = {dataset: torch.load('./Utils/TrainingSplits/testIDs.pt')[dataset]}
-        trainSetMoments = getDatasetMoments(trainIDs)
-        testSetMoments = getDatasetMoments(testIDs)
+
+    ## Get Statistics of datasets
+    trainSetMoments = getDatasetMoments(trainIDs)
+    testSetMoments = getDatasetMoments(testIDs)
 
     ## Parallelize net
     if torch.cuda.device_count() > 1:
         net = torch.nn.DataParallel(net)
 
-    ##Change Optimizer params
-    #for g in optimizer.param_groups:
-    #    g['lr'] = 0.05
-        #g['momentum'] = 0.9
-
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8, patience=50, verbose=True)
-
 
     ## Instantiate Training and Validation DataLoaders
     trainDataSet = YeastSegmentationDataset(trainIDs, crop_size = 256, random_rotate = True, random_flip = False,
@@ -130,10 +96,10 @@ def main():
     testLoader = torch.utils.data.DataLoader(testDataSet, batch_size=1,
                                             shuffle=False, num_workers=0)
 
-
     ## Set Training hyperparameters/conditions
     criterion = WeightedCrossEntropyLoss()
     classes = ('background','cell')
+
     ## Epoch Loop: first loops over batches, then over validation set
     for epoch in range(start,end):  
         
@@ -152,9 +118,6 @@ def main():
             ## Forward Pass
             outputs = net(trainingImage.float())
 
-            ## Write Graph
-            #writer.add_graph(net, trainingImage.float())
-
             ## Calculate and Write Loss
             if torch.isnan(outputs).any() > 0:
                 print('There Be Nans')
@@ -171,7 +134,6 @@ def main():
 
         val_acc = validate(net, device, testLoader, criterion, saveImages = False)
         scheduler.step(val_acc)
-        #print('Current LR is: ' + str(optimizer.param_groups['lr']))
         
         print('[%d, %d] IntOfUnion (Cell): %.5f \n' % (iteration, epoch + 1, val_acc),flush=True)
         writer.add_scalar('Validation Cell IOU', val_acc, global_step=epoch, walltime=time.time())
@@ -201,10 +163,6 @@ def main():
                 "iteration": iteration,
                 "highestAccuracy": val_acc,
             }
-            #if allDatasets == 'False':
-            #    outputpath ="./NewNormModels/new_norm_testDSV%01dK%01d.pt" % (normtype,k) 
-            #else:
-            #    outputpath ="./NewNormModels/new_norm_testV%01dK%01d.pt" % (normtype,k) 
             
             outputpath = '{}_w0={}_sigma={}.pt'.format(dataset, loss_param[0], loss_param[1])
             torch.save(checkpoint, outputpath)
@@ -216,5 +174,32 @@ def main():
         outputfile.writelines('w0={},sigma={},accuracy={:.4f}\n'.format(loss_param[0], loss_param[1], 
                                                                 checkpoint['highestAccuracy']))
     writer.close()
+
 if __name__ == '__main__':
+
+## Load Settings YAML file
+    if os.path.exists("settings.yml"):        
+        settings=yaml.load(open("settings.yml","r"))
+    train_param = {}
+## Try loading settings from environment variable
+    try:
+        k = int(os.environ['K_FOLD'])
+        toResume = os.environ['RESUME']
+        normtype = os.environ['NORMTYPE']
+        allDatasets = os.environ['ALLDATASETS']
+        loss_param = [os.environ['W0'],os.environ['SIGMA']]
+        dataset = os.environ['DATASET']
+    except KeyError:
+        allDatasets = settings['data']['allDatasets']
+        loss_param = settings['data']['loss_param']
+        k = settings['train_param']['k']
+        toResume = settings['train_param']['toResume']
+        normtype = settings['train_param']['normtype']
+
+    loss_param = ['10','5'] if dataset == 'DSDataset' else ['5','5']
+
+    end = settings['train_param']['end']
+    lr = settings['train_param']['learning_rate']
+    momentum = settings['train_param']['momentum']
+
     main()
